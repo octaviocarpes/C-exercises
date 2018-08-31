d411b1ea67b7facb51893089c7f1abb16a205df2
13202108 <13202108@facin.portoalegre.pucrsnet.br>
2018-08-31 20:47:28 -0300

    Added exercise 3

---
+++ 2nd Exercise List/Exercise 3/Exercise 3/main.c
@@ -0,0 +1,17 @@
+//3. Escreva uma função similar à anterior, mas que conta e retorna a quantidade de palavras
+//na linha de texto.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int count(char words[100]) {
+    int size =
+}
+
+int main()
+{
+    char words[100] = "So many beautiful words!";
+    count(words)
+    return 0;
+}
